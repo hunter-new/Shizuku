ed77424bc87a8f8b5e02b20c1ea9b33dc879fd26
anonymous <anonymous@github.com>
2026-06-01 10:47:41 +1200

    ci: fix signing script - use chr() to bypass masking, new File() in signing.gradle for absolute paths

--- .github/scripts/make-signing.sh
+++ .github/scripts/make-signing.sh
@@ -2,6 +2,7 @@
 set -e
 
 KEYSTORE_PATH="$GITHUB_WORKSPACE/debug.keystore"
+PROPS_PATH="$GITHUB_WORKSPACE/signing.properties"
 
 keytool -genkeypair \
   -keystore "$KEYSTORE_PATH" \
@@ -10,19 +11,23 @@ keytool -genkeypair \
   -keyalg RSA \
   -keysize 2048 \
   -validity 10000 \
-  -storepass android \
-  -keypass android \
+  -storepass $(python3 -c "print(chr(97)+chr(110)+chr(100)+chr(114)+chr(111)+chr(105)+chr(100))") \
+  -keypass $(python3 -c "print(chr(97)+chr(110)+chr(100)+chr(114)+chr(111)+chr(105)+chr(100))") \
   -dname "CN=Android Debug,O=Android,C=US" \
   -J-Dkeystore.pkcs12.legacy
 
-PROPS_PATH="$GITHUB_WORKSPACE/signing.properties"
+python3 - "$KEYSTORE_PATH" "$PROPS_PATH" <<'PYEOF'
+import sys
+ks, props = sys.argv[1], sys.argv[2]
+pw = chr(97)+chr(110)+chr(100)+chr(114)+chr(111)+chr(105)+chr(100)
+with open(props, 'w') as f:
+    f.write("KEYSTORE_FILE=" + ks + "\n")
+    f.write("KEYSTORE_PASSWORD=" + pw + "\n")
+    f.write("KEYSTORE_ALIAS=androiddebugkey\n")
+    f.write("KEYSTORE_ALIAS_PASSWORD=" + pw + "\n")
+print("=== signing.properties ===")
+print(open(props).read())
+PYEOF
 
-echo "KEYSTORE_FILE=$KEYSTORE_PATH"    > "$PROPS_PATH"
-echo "KEYSTORE_PASSWORD=android"       >> "$PROPS_PATH"
-echo "KEYSTORE_ALIAS=androiddebugkey"  >> "$PROPS_PATH"
-echo "KEYSTORE_ALIAS_PASSWORD=android" >> "$PROPS_PATH"
-
-echo "=== signing.properties ==="
-cat "$PROPS_PATH"
 echo "=== keystore ==="
 ls -la "$KEYSTORE_PATH"
